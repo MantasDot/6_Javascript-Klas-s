53d98e84da3f93b238d743792c2528e7203ef24c
MANTAS_JUODELIS <MANtas.juodelis1@gmail.com>
2021-06-29 09:42:39 +0300

    pradzia

---
+++ scriaaapt.js
@@ -0,0 +1,14 @@
+/*function Namas(Miestas, pastatymo_data, adresas, laiptinių_skaičius, butų_skaičius, bendras_butų_skaičius) {
+      
+ 
+
+    this.Miestas = Miestas;
+    this.pastatymo_data = data();
+    this.adresas = adresas;
+    this.laiptinių_skaičius = [l1,l2,l3,l4,l5];
+    this.butų_skaičius = butų_skaičius;
+    this.bendras_butų_skaičius = bendras_butų_skaičius;
+ 
+  console.log(Namas());
+     }
+ }*/
