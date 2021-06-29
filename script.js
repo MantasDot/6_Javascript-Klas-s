57e8399194d9a059c37116c7120907afcfbda830
MANTAS_JUODELIS <MANtas.juodelis1@gmail.com>
2021-06-29 09:41:51 +0300

    pradzia

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+class Namas {
+    constructor (Miestas, pastatymo_data, adresas, laiptinių_skaičius, butų_skaičius, bendras_butų_skaičius) {
+        this.Miestas = Miestas;
+        this.adresas = adresas;
+        this.pastatymo_data = pastatymo_data;
+        this.laiptinių_skaičius = laiptinių_skaičius;
+        this.butų_skaičius = butų_skaičius;
+        this.bendras_butų_skaičius = bendras_butų_skaičius;
+    }
+
+    
+    }
+        
+    
+
